2c93b03027e90a748e5626dbc5b055be8c023e3f
Swouterke <wvercruysse45@gmail.com>
2018-04-24 10:42:19 +0200

    Afgewerkte Applicatie QuickAccesSettingsMRU

---
+++ MyConverters.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace WpfCurcus
+{
+    public class BooleanToFontWeight : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Boolean)value)
+            {
+                return "Bold";
+            }
+            else
+            {
+                return "Normal";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class BooleanToFontStyle : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Boolean)value)
+                return "Italic";
+            else
+                return "Normal";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- WindowMetRibbon.xaml.cs
+++ WindowMetRibbon.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.Collections.Specialized;
 
 namespace WpfCursus
 {
@@ -29,9 +30,57 @@ namespace WpfCursus
         public WindowMetRibbon()
         {
             InitializeComponent();
+            LeesMRU();
+            StringCollection qatLijst = new StringCollection();
+            if (WpfCurcus.Properties.Settings.Default.qat != null)
+            {
+                qatLijst = (StringCollection)WpfCurcus.Properties.Settings.Default.qat;
+                int lijnNr = 0;
+                while (lijnNr < qatLijst.Count)
+                {
+                    String commando = qatLijst[lijnNr];
+                    String png = qatLijst[lijnNr + 1];
+                    RibbonButton nieuweKnop = new RibbonButton();
+                    BitmapImage icon = new BitmapImage();
+                    icon.BeginInit();
+                    icon.UriSource = new Uri(png);
+                    icon.EndInit();
+                    nieuweKnop.SmallImageSource = icon;
+
+                    CommandBindingCollection ccol = this.CommandBindings;
+                    foreach(CommandBinding cb in ccol)
+                    {
+                        RoutedUICommand rcb = (RoutedUICommand)cb.Command;
+                        if (rcb.Text == commando)
+                            nieuweKnop.Command = rcb;
+                    }
+                    Qat.Items.Add(nieuweKnop);
+                    lijnNr += 2;
+                }
+            }
         }
 
+        private void RibbonWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StringCollection qatLijst = new StringCollection();
+            if (WpfCurcus.Properties.Settings.Default.qat != null)
+                WpfCurcus.Properties.Settings.Default.qat.Clear();
+            foreach (Object li in Qat.Items)
+            {
+                if (li.GetType() == typeof(RibbonButton))
+                {
+                    RibbonButton knop = (RibbonButton)li;
+                    RoutedUICommand commando = (RoutedUICommand)knop.Command;
+                    qatLijst.Add(commando.Text);
+                    qatLijst.Add(knop.SmallImageSource.ToString());
+                }
+            }
+            if (qatLijst.Count > 0)
+                WpfCurcus.Properties.Settings.Default.qat = qatLijst;
+            WpfCurcus.Properties.Settings.Default.Save();
+        }
 
+        
         private void LeesBestand(string bestandsnaam)
         {
             try
@@ -40,6 +89,7 @@ namespace WpfCursus
                 {
                     TextBoxVoorbeeld.Text = bestand.ReadLine();
                 }
+                BijwerkenMRU(bestandsnaam);
             }
             catch (Exception ex)
             {
@@ -74,6 +124,7 @@ namespace WpfCursus
                         bestand.WriteLine(TextBoxVoorbeeld.Text);
                     }
                 }
+                BijwerkenMRU(dlg.FileName);
             }
             catch (Exception ex)
             {
@@ -108,6 +159,50 @@ namespace WpfCursus
         private void HelpExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             MessageBox.Show("Dit is helpscherm", "Help", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+        }
+
+        private void MRUGallery_SelectionChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            LeesBestand(MRUGallery.SelectedValue.ToString());
+        }
+
+        private void Radio_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton keuze = (RibbonRadioButton)sender;
+            BrushConverter bc = new BrushConverter();
+            SolidColorBrush kleur = (SolidColorBrush)bc.ConvertFromString(keuze.Tag.ToString());
+            TextBoxVoorbeeld.Foreground = kleur;
+        }
+
+        private void LeesMRU()
+        {
+            StringCollection mruLijst = new StringCollection();
+            MRUGalleryCat.Items.Clear();
+            if (WpfCurcus.Properties.Settings.Default.mru != null)
+            {
+                mruLijst = (StringCollection)WpfCurcus.Properties.Settings.Default.mru;
+                for (int lijnNr = 0; lijnNr < mruLijst.Count; lijnNr++)
+                {
+                    MRUGalleryCat.Items.Add(mruLijst[lijnNr]);
+                }
+            }
+        }
+        private void BijwerkenMRU(string bestandsnaam)
+        {
+            StringCollection mruLijst = new StringCollection();
+            if (WpfCurcus.Properties.Settings.Default.mru != null)
+            {
+                mruLijst = (StringCollection)WpfCurcus.Properties.Settings.Default.mru;
+                int positie = mruLijst.IndexOf(bestandsnaam);
+                if (positie >= 0)
+                    mruLijst.RemoveAt(positie);
+                else
+                    if (mruLijst.Count >= 6) mruLijst.RemoveAt(5);
+            }
+            mruLijst.Insert(0, bestandsnaam);
+            WpfCurcus.Properties.Settings.Default.mru = mruLijst;
+            WpfCurcus.Properties.Settings.Default.Save();
+            LeesMRU();
         }
     }
 }
